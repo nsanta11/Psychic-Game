e75bc709f55f1250f41986875f65013184feb957
Nicole Santarsiero <nicolesanta@Nicoles-MacBook.local>
2018-02-17 08:50:57 -0700

    HW Assignment 3

---
+++ assets/javascript/game.js
@@ -0,0 +1,63 @@
+
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var guessList = [];
+
+
+//must call upon the computer to choose first and this letter can only last until guesses left < 1
+$(document).ready(function computerChoice() {
+
+    var computerLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+    var html =
+          "<p>You chose: " + guessList + "</p>" +
+          //"<p>The computer chose: " + computerLetter + "</p>" +
+          "<p>Wins: " + wins + "</p>" +
+          "<p>Losses: " + losses + "</p>" +
+          "<p>Guesses: " + guessesLeft + "</p>";
+        // Set the inner HTML contents of the #game div to our html string
+        document.querySelector("#game").innerHTML = html;
+
+        console.log("The computer chose: " + computerLetter);
+
+
+    //need to add the uppercase conversion to lowercase!! and to only allow for letter entries!!!
+  document.onkeyup = function(event) {
+
+    var userGuess = event.key;
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase(); 
+
+      //Loop for guessing cycle)
+      if (userGuess === computerLetter) {
+        wins++;
+        guessesLeft = 9;
+        computerChoice();
+        guessList[guessList.length]=userGuess;
+        guessList = [];
+        } 
+      else if (guessesLeft > 1) {
+        guessesLeft--;
+        guessList[guessList.length]=userGuess;
+        }
+      else if (guessesLeft <= 1) {
+        losses++;
+        guessesLeft = 9;
+        computerChoice();
+        guessList = [];
+        }
+
+     var html =
+          "<p>You chose: " + guessList + "</p>" +
+          //"<p>The computer chose: " + computerLetter + "</p>" +
+          "<p>Wins: " + wins + "</p>" +
+          "<p>Losses: " + losses + "</p>" +
+          "<p>Guesses: " + guessesLeft + "</p>";
+        // Set the inner HTML contents of the #game div to our html string
+        document.querySelector("#game").innerHTML = html;
+
+  }
+
+})
